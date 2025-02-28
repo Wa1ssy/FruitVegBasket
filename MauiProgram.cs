7a851f4a3516495b19fca4c53fa7de3bde7158a4
opilane <opilane@HP-CZC0359NNB>
2025-02-28 14:00:23 +0200

    Part 2 tehtud

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
-
+using FruitVegBasket.Pages;
+using FruitVegBasket.Services;
+using FruitVegBasket.ViewModels;
 namespace FruitVegBasket;
 
 public static class MauiProgram
@@ -12,13 +14,19 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Ubuntu-Regular.ttf", "UbuntuRegular");
+				fonts.AddFont("Ubuntu-Bold.ttf", "UbuntuBold");
 			})
             .UseMauiCommunityToolkit();
 
+        builder.Services.AddSingleton<CategoryService>();
+        builder.Services.AddSingleton<HomePageViewModel>();
+        builder.Services.AddSingleton<HomePage>();
+
+
+
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
