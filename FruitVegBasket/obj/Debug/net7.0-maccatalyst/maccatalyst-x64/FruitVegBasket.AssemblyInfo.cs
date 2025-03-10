03133e8c9dae669d813cacd614d073413ed458a0
Opilane TTHK <132674823+Wa1ssy@users.noreply.github.com>
2025-03-10 13:44:29 +0200

    Part 5 done, but cant do the migration

--- FruitVegBasket/obj/Debug/net7.0-maccatalyst/maccatalyst-x64/FruitVegBasket.AssemblyInfo.cs
+++ FruitVegBasket/obj/Debug/net7.0-maccatalyst/maccatalyst-x64/FruitVegBasket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FruitVegBasket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+13b1f7502dcb97ea76d8aeca42748f8e1aa79b98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+eb4a1ed2b2bee9cc3c3e838bfc18cef187d04bc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FruitVegBasket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FruitVegBasket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
